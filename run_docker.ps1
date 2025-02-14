7244209ac4afd141d3a35cdf90fc5e7f554eebc5
Taha Ansari <tahaaansari@hotmail.com>
2025-02-15 00:37:30 +1100

    fix: better dockerfile and docker run scripts

--- run_docker.ps1
+++ run_docker.ps1
@@ -3,7 +3,5 @@ docker build -t vae-pruning .
 
 # Run the container with GPU support and volume mounting
 docker run --gpus all `
-    -v ${PWD}/output:/app/output `
-    -v ${PWD}/images:/app/images `
-    -v ${PWD}/vae_pruning_analysis.py:/app/vae_pruning_analysis.py `
+    -v ${PWD}:/app `
     vae-pruning
